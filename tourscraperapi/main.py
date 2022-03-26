14ca22b30711ef30b22a2725fff2b4193e6d9635
Dalton Black <5530804+daltonmblack@users.noreply.github.com>
2022-03-25 19:14:42 -0700

    Migrate from pipenv to poetry (#1)

--- tourscraperapi/main.py
+++ tourscraperapi/main.py
@@ -1,6 +1,6 @@
-from optparse import Option
 from typing import Optional
 
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -22,3 +22,7 @@ def read_item(item_id: int, q: Optional[str] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
   return {"item_name": item.name, "item_id": item_id}
+
+def start():
+  """Launched with `poetry run start` at root level"""
+  uvicorn.run("tourscraperapi.main:app", host="0.0.0.0", port=8000, reload=True)
